e8251d010b1268da61bc142603badb99960da6b3
chavdar <chavdar.rakov@gmail.com>
2020-11-18 14:53:28 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,11 @@
 provider "aws" {
   region = "ap-northeast-2"
-  #region = "us-west-2"
 }
-
+# Additional provider configuration for west region
 provider "aws" {
   alias  = "london"
   region = "eu-west-2"
 }
-
-provider "aws" {
-  alias  = "canada"
-  region = "ca-central-1"
-}
-
 
 data "aws_ami" "ubuntu" {
   most_recent = true
@@ -48,18 +41,15 @@ data "aws_ami" "ubuntu_london" {
   owners = ["099720109477"] # Canonical
 }
 
-
-
 variable "instance_type" {
-  type = string
+  type    = string
   default = "t3.micro"
-  #default = "m5.24xlarge"
 }
-
 
 resource "aws_instance" "seoul" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
+
 
   tags = {
     Name = "seoul"
